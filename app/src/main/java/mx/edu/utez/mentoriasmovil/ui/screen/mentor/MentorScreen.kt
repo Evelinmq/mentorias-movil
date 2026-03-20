bd92c6e9352af119311a0f22c2a7da76cef9b329
Andreaara <20243ds171@utez.edu.mx>
2026-03-19 19:59:39 -0600

    Mentor se agrego calendario en la pantalla de mentor

--- app/src/main/java/mx/edu/utez/mentoriasmovil/ui/screen/mentor/MentorScreen.kt
+++ app/src/main/java/mx/edu/utez/mentoriasmovil/ui/screen/mentor/MentorScreen.kt
@@ -1,11 +1,25 @@
 package mx.edu.utez.mentoriasmovil.ui.screen.mentor
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,27 +28,59 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import mx.edu.utez.mentoriasmovil.ui.components.AddButton
 import mx.edu.utez.mentoriasmovil.ui.components.MainHeader
-import mx.edu.utez.mentoriasmovil.ui.nav.AdminBottomBar
+
 import mx.edu.utez.mentoriasmovil.ui.theme.MentoriasMovilTheme
-import mx.edu.utez.mentoriasmovil.viewmodel.MentorViewModel
+
 import androidx.compose.runtime.getValue
+
 import androidx.compose.runtime.setValue
 
+import androidx.compose.ui.graphics.Color
+
+
 @Composable
-fun MentorScreen(viewModel: Unit) {
+fun MentorScreen() {
 
-    var showAddDialog by remember { mutableStateOf(false)}
+    var showAddDialog by remember { mutableStateOf(false) }
 
-    Spacer(modifier = Modifier.height(34.dp))
+    //estado del calendario
+    val dateEstado = rememberDatePickerState()
 
-    Box(modifier = Modifier.padding(horizontal = 16.dp)) {
-        AddButton (onClick = { showAddDialog = true })
+    Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+
+        Spacer(modifier = Modifier.height(34.dp))
+
+        AddButton(onClick = { showAddDialog = true })
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            color = Color(0xFFE8E7E7),
+            tonalElevation = 2.dp
+        ) {
+            DatePicker(
+                state = dateEstado,
+                title = null,
+                headline = null,
+                showModeToggle = false
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        val fechaMs = dateEstado.selectedDateMillis
+        if (fechaMs != null) {
+            Text(text = "Fecha seleccionada: ${java.util.Date(fechaMs)}")
+        }
     }
 
-
-
-
 }
+
+
+
+
 
 @Preview(showBackground = true)
 @Composable
@@ -43,8 +89,11 @@ fun PreviewMentor() {
         Scaffold (
             topBar = { MainHeader (onLogout = {})}
         ) { paddingValues ->
-            Column(modifier = Modifier.padding(paddingValues)) {
-                MentorScreen(viewModel = Unit)
+            Surface(modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+                color = MaterialTheme.colorScheme.background) {
+                MentorScreen()
             }
         }
     }
