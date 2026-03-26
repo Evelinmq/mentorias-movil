c8dfbd714af399a84c33f2f0e17f4f89c7f14803
GetseCruz <20243ds149@utez.edu.mx>
2026-03-25 18:17:47 -0600

    eliminar

--- app/src/main/java/mx/edu/utez/mentoriasmovil/data/model/LoginRequest.kt
+++
@@ -1,8 +0,0 @@
-package mx.edu.utez.mentoriasmovil.data.model
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginRequest (
-    @SerializedName("nombreUsuario") val nombreUsuario: String,
-    @SerializedName("password") val contrasena: String
-)
